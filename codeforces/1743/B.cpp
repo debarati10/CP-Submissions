25ccbe397738d212a6bd4a5a2820283a7f174b58
Debarati Chakrabarti <debaratichakrabarti10@gmail.com>
2022-10-17 20:24:00 +0530

    Add solution for problem `B - Permutation Value` Link: https://codeforces.com/contest/1743/problem/B Tags: constructive algorithms, greedy, *800 Ref: https://codeforces.com/contest/1743/submission/176721433

---
+++ codeforces/1743/B.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int test;
+    cin>>test;
+    while(test--){
+        int number;
+        cin>>number;
+        for(int j=2;j<=number;j++) cout<<j<<" ";
+        cout<<1<<endl;
+    }
+}
