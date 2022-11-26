6e1164bc96818c5595479656ee8d93e9f7c649e6
Debarati Chakrabarti <debaratichakrabarti10@gmail.com>
2022-11-26 20:04:00 +0530

    Add solution for problem `A - Doremy's Paint` Link: https://codeforces.com/contest/1764/problem/A Tags: greedy, *800 Ref: https://codeforces.com/contest/1764/submission/182668242

---
+++ codeforces/1764/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#define long long ll;
+using namespace std;
+
+int main() {
+    int testcos;
+    cin>>testcos;
+    while(testcos--){
+        int lenarr;
+        cin>>lenarr;
+        int arr[lenarr];
+        int n=lenarr;
+        for(int i=1;i<=n;i++){
+            cin>>arr[i];
+        }
+        cout<<"1"<<" "<<n<<endl;
+    }
+}
