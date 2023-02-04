b5519eaa94b9f7b2fd860a66478b9febca02be3c
Debarati Chakrabarti <debaratichakrabarti10@gmail.com>
2023-02-04 20:22:00 +0530

    Add solution for problem `A - Bit++` Link: https://codeforces.com/contest/282/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/282/submission/192165316

--- codeforces/282/A.cpp
+++ codeforces/282/A.cpp
@@ -4,16 +4,20 @@
 using namespace std;
  
 int main() {
-    int n;
+    int n, ans=0;
     cin>>n;
-    int ans=0;
-    while(n--){
-        string s;
-        cin>>s;
-        if(s=="++X" || s=="X++")
+    string s2="++X";
+    string s3="X++";
+    string s4="--X";
+    string s5="X--";
+    string s1;
+    for(int i=0;i<n;i++){
+        cin>>s1;
+        if(s1==s2 || s1==s3)
         ans+=1;
-        if(s=="--X" || s=="X--")
+        if(s1==s4 || s1==s5)
         ans-=1;
     }
+    
     cout<<ans;
 }
